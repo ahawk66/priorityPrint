66939f85f8a7df3d5920d701a9ade39ef7649c3c
Tyler Bailey <tylerdavidbailey@gmail.com>
2016-02-07 22:23:47 -0500

    Update sim.cpp
    tyler changed this

--- sim.cpp
+++ sim.cpp
@@ -1,12 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
 #include "simulation.h"
 
-int printJobType::getNumPages()
+using namespace std;
+
+printJobType::printJobType(int nPages, int aTime)
 {
-    
+	setNumPages(nPages);
+	setArrivalTime(aTime);
 }
 
-void printJobType::setNumPages(int n)
+int getNumPages()
 {
-    
+	return numPages;
 }
-////////
+
+void setNumPages(int n)
+{
+	numPages = n;
+}
+
+int getArrivalTime()
+{
+	return arrivalTime;
+}
+
+void setArrivalTime(int t)
+{
+	arrivalTime = t;
+}
