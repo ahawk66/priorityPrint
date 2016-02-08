468716d20708a2d44ec06be1b5054b7c69fcf4fe
ahawk66 <ghawk663@gmail.com>
2016-02-07 21:53:53 -0500

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -1,1 +1,43 @@
 //yea theres nothing atm but this is the header file 
+#ifndef simulation_hpp
+#define simulation_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <queue>
+#include <fstream>
+
+
+class printerType
+{
+public:
+    printerType();
+    
+private:
+    int printSpeed;
+    bool busy;
+    
+};
+
+
+
+
+
+
+class printJobType
+{
+public:
+    
+private:
+    int numPages;
+};
+
+
+
+
+#endif /* simulation_hpp */
+
