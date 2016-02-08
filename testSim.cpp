29c169103c49d37e18798c99a910eb674fa84293
Tyler Bailey <tylerdavidbailey@gmail.com>
2016-02-07 21:11:26 -0500

    Create testSim.cpp
    v1

---
+++ testSim.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <queue>
+#include <fstream>
+
+using namespace std;
+
+void runSim();
+
+int main()
+{
+    runSim();
+    return 0;
+}
+
+
+void runSim()
+{
+    int maxNumOfPages = 1;
+    int pagesPerMinute = 1;
+    int numberOfPrintJobs = 1;
+    int numberOfPrintersBeingUsed = 1;
+    int seed = 0;
+
+    cout << "Max number of pages per job: ";
+    cin >> maxNumOfPages;
+    if (maxNumOfPages < 1)
+        maxNumOfPages = 1;
+    cout << endl;
+
+    cout << "Pages per minute: ";
+    cin >> pagesPerMinute;
+    if (pagesPerMinute < 1)
+        pagesPerMinute = 1;
+    cout << endl;
+
+    cout << "Print jobs: ";
+    cin >> numberOfPrintJobs;
+    if (numberOfPrintJobs < 1)
+        numberOfPrintJobs = 1;
+    cout << endl;
+
+    cout << "Number of printers being used: ";
+    cin >> numberOfPrintersBeingUsed;
+    if (numberOfPrintersBeingUsed < 1)
+        numberOfPrintersBeingUsed = 1;
+    cout << endl;
+    
+    cout << "Do you want to enter a seed? y or n?";
+    string yesOrNo = "n";
+    cin >> yesOrNo;
+    if (yesOrNo == "y" || yesOrNo == "Y")
+    {
+        cout << "Enter the seed: ";
+        cin >> seed;
+    }
+    cout << endl;
+
+
+    //queue<int> printJobs;
+    int randPageNum;
+    for (int i = 0; i < numberOfPrintJobs; ++i)
+    {
+        randPageNum = rand() % maxNumOfPages +1;
+        printJobs.push(randPageNum);
+
+        cout << "Print Job Added [" << randPageNum << "]" << endl;
+    }
+
+}
