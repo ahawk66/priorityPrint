b13a51e25c4e16c038a8fd5dcd431cbd48208eaa
naylorgang <mtownnaylor@gmail.com>
2016-02-22 19:35:04 -0500

    Update simulation.h

--- simulation.h
+++ simulation.h
@@ -2,83 +2,113 @@
 #include <string>
 #include <queue>
 
+
 using namespace std;
 
-class printJobType
+
+/////////////////////////////////////////
+//  PrintJob stores and/or modifies information about what needs to be printed, how much still needs to be printed, and what time it got to a printer,
+/////////////////////////////////////////
+class PrintJob
 {
 public:
-	printJobType(int i,int nPages, int aTime);
+	PrintJob(int identifier =-1,int nPages = -1, int aTime= -1);
 
 	int getNumPages();
-
-	void setNumPages(int n);
-
 	int getArrivalTime();
-
-	void setArrivalTime(int t);
-
+	int getRemainingPages();
 	int getId();
-
-	void setId(int i);
+	void decrementPages(int pagesToPrint);
 
 private:
 	int numPages;
 	int arrivalTime;
+	int remainingPages;
 	int id;
 };
 
-/////////////////////////////////////////
-
-
-class printerListType
+class PrintJobQueue: public queue<PrintJob>
 {
 public:
-	printerListType(int nPrinters);
+	PrintJobQueue();
+	
+};
 
-	int setNumPrinters(int n);
+/////////////////////////////////////////
+//  JobQueueManager will hold and manage the job queues
+/////////////////////////////////////////
+class JobQueueManager
+{
+	public:
+	JobQueueManager(); // Constructor will ] generate the queues
+	int addJob(PrintJob job); //newJob takes a printjob, sends it to a print queue, and returns queue id
+	PrintJob getJob();
+	bool hasJob();
+	int getNumJobs();
+	private:
+	PrintJobQueue* jobQueueOne; // array for jobqueues
+	PrintJobQueue* jobQueueTwo;
+	PrintJobQueue* jobQueueThree;
+	
+};
 
-	void getNumPrinters();
+/////////////////////////////////////////
+//  Printer gets jobs, has a certain speed, and holds jobs.
+/////////////////////////////////////////
+class Printer
+{
+public: 
+	Printer(int identifier = -1, int speed=-1);
 
-	bool isPrinterOpen(PrinterType p);
+	void sendJob(PrintJob newJob);
 
-	PrinterType getOpenPrinter();
+	bool isOpen();
 
-	//void decrementPages();
+	void decrementPages();
+
+	int getId();
+
+	int getPrinterSpeed();
+	PrintJob getCurrentPrintJob();
+
+
+private:
+	int printerSpeed;
+	int id;
+	PrintJob currentJob;
+
+};
+
+/////////////////////////////////////////
+//  The printer list holds and generates all the printers and will tell you if they are open and how many we have.
+//  It also holds the key function to update the printers(fetch and process jobs)
+/////////////////////////////////////////
+class PrinterList
+{
+public:
+	PrinterList(int nPrinters, int speed);
+	
+	int updatePrinters(int clock, JobQueueManager queueManager);
+
+	int getNumPrinters();
+
+	bool isPrinterOpen();
+
+	Printer getOpenPrinter();
+
 	
 private:
 	int numPrinters;
-	PrinterType * printerArray;
+	Printer * printerArray;
 	// to allocate in constructor do printerArray= new printerType[];
 };
 
 
 /////////////////////////////////////////
 
-class printerType
-{
-public: 
-	printerType(int i, int printerSpeed);
-
-	void addJob(printJobType newJob);
-
-	bool isEmpty();
-	void setIsEmpty(bool isE);
-
-	void decrementPages();
-
-	int getId();
-	void setId(int i);
-
-	void setPrinterSpeed(int ps);
-	int getPrinterSpeed();
 
 
-private:
-	int printerSpeed;
-	int id;
-	bool isEmpty;
 
-}
 
 
 
